f090f4e3e83e64bb0c65e5da3bc34016891b1494
Shawn Lambert <shawnlambert004@gmail.com>
2025-08-26 13:56:03 +0100

    Feed & Repo FindB

--- src/main/java/com/shawn/financial_hub/UserRepo.java
+++ src/main/java/com/shawn/financial_hub/UserRepo.java
@@ -2,6 +2,7 @@ package com.shawn.financial_hub;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepo
