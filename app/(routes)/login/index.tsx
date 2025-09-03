61e24a7a496558144f5ac6c78a4a099099565c96
Shawn Lambert <shawnlambert004@gmail.com>
2025-09-03 20:58:34 +0100

    Username/password form complete

--- app/(routes)/login/index.tsx
+++ app/(routes)/login/index.tsx
@@ -24,24 +24,24 @@ export default function LoginScreen() {
               style={stylist.OnboardPage} />
       <KeyboardAvoidingView
         behavior = {Platform.OS == "ios"? "padding": "height"}
-        style={{flex: 1, paddingBottom: 50, paddingTop: 250, paddingHorizontal: 20, zIndex : 2}}
+        style={{flex: 1, paddingBottom: 50, paddingTop: 50, paddingHorizontal: 20, zIndex : 2}}
         >
-            <Text style={stylist.title}>Welcome</Text>
+            <Text style={[stylist.title, {marginTop: 90}]}>Welcome</Text>
             <Text style={stylist.title}>back</Text>
-            <Text style={[stylist.userlog, {marginTop: 80}]}>
+            <Text style={[stylist.userlog, {marginTop: 50}]}>
               Username
             </Text>
             <Controller 
             control={loginForm.control}
             name="username"
             render = {({field: {onChange, onBlur, value}}) => (
-              <View style={tw`flex-row items-center bg-black-50 rounded-xl px-4 py-4 border ${loginForm.formState.errors.mail ? 'border-red-500' : 'border-gray-200'}`}
+              <View style={tw`flex-row items-center bg-black-50 rounded-xl px-3 py-4 border ${loginForm.formState.errors.username ? 'border-red-500' : 'border-gray-200'}`}
               >
               
               <MaterialCommunityIcons
               name= "account-outline"
               size={20}
-              color="black"
+              color="white"
               />
               <TextInput 
               style = {tw `flex-1 ml-3 text-gray-800 font-NewsReader`}
@@ -49,6 +49,32 @@ export default function LoginScreen() {
               value={value}
               onChangeText={onChange}
               onBlur={onBlur}
+              />
+              </View> )} />
+
+              <Text style={[stylist.userlog, {marginTop: 30}]}>
+                Password
+              </Text>
+
+              <Controller
+              control={loginForm.control}
+              name="password"
+              rules={{required: "password is required",}}
+              render={({field: {onChange, onBlur, value}}) => (
+              <View style={tw`flex-row items-center bg-black-50 rounded-xl px-3 py-4 border ${loginForm.formState.errors.password ? 'border-red-500' : 'border-gray-200'}`}
+              >
+                <MaterialCommunityIcons 
+                name="lock"
+                size={20}
+                color = "white"
+                />
+                <TextInput 
+              style = {tw `flex-1 ml-3 text-gray-800 font-NewsReader`}
+              placeholder='Password'
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              secureTextEntry= {true}
               />
               </View>
             )} 
