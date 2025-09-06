c27e1b9d283a6a5f4e827ed05bb1ad2071f0c149
Shawn Lambert <shawnlambert004@gmail.com>
2025-09-06 19:06:58 +0100

    Dashboard Page Titles

--- app/(routes)/Dashboard/index.tsx
+++ app/(routes)/Dashboard/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Dashboard() {
   return (
     <View>
-        <Text style={[{color:"white"}]}>Dashboard Hello</Text>
+        <Text style={stylist.title}> Dashboard Hello </Text>
     </View>
   )
+
 }
+
+    const stylist = StyleSheet.create({
+    title: {
+        fontFamily : 'NewsReader',
+        fontSize: 20,
+        color: '#000000',
+        marginBottom: 10,
+        textAlign: 'center',
+    }
+  })
